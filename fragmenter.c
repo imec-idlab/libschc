f3a980a4d26f97771805102760af5bb69519c603
boortmans <bart.moons@gmail.com>
2018-09-05 15:11:40 +0200

    bitmap

--- fragmenter.c
+++ fragmenter.c
@@ -1272,12 +1272,13 @@ int8_t schc_reassemble(schc_fragmentation_t* rx_conn) {
 	rx_conn->ack.fcn = fcn;
 
 	set_conn_fcn(rx_conn, fcn); // set rx_conn->fcn
-	rx_conn->frag_cnt = rx_conn->fcn * (rx_conn->window_cnt + 1);
 
 	if (window == (!rx_conn->window)) {
 		DEBUG_PRINTF("window_cnt++ \n");
 		rx_conn->window_cnt++;
 	}
+
+	rx_conn->frag_cnt = rx_conn->fcn * (rx_conn->window_cnt + 1);
 	tail->frag_cnt = (rx_conn->fcn + (get_max_fcn_value() * rx_conn->window_cnt)); // set frag_cnt belonging to mbuf
 
 	if(rx_conn->RX_STATE != END_RX) {
